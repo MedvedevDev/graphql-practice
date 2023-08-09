a7a294b5cafe23882d00871fd6f8401abbf0f01c
MedvedevDev <a95.medvedev@gmail.com>
2023-08-09 22:07:27 +0300

    Add relationships between user, post and comment schemas

--- blogg-app-2/src/index.js
+++ blogg-app-2/src/index.js
@@ -1,10 +1,84 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const users = [{
+        id: 1,
+        name: "String",
+        email:"String",
+        age: 12
+},
+    {
+        id: 2,
+        name: "String",
+        email:"String",
+        age: 11
+    },
+    {
+        id: 3,
+        name: "String 3",
+        email:"String 3",
+        age: 10
+    }
+]
+
+const comments = [
+    {
+        id: 1,
+        comment: "string string",
+        author: 1,
+        post: 1
+    },
+    {
+        id: 2,
+        comment: "string string",
+        author: 2,
+        post: 1
+    },
+    {
+        id: 3,
+        comment: "string string",
+        author: 2,
+        post: 2
+    },
+    {
+        id: 4,
+        comment: "string string",
+        author: 3,
+        post: 3
+    }
+]
+
+const posts = [{
+    id: 1,
+    title: "2",
+    body: "strng",
+    published: false,
+    author: '1'
+},
+    {
+        id: 2,
+        title: "strng 2",
+        body: "strng 2",
+        published: true,
+        author: '1'
+    },
+    {
+        id: 3,
+        title: "2",
+        body: "strng",
+        published: true,
+        author: '2'
+    }
+]
+
 // Type definitions (schema)
 const typeDefs = gql`
     type Query {
+        add(a: Float!, b: Float!): Float!
         me: User!
         post: Post!
+        users: [User!]!
+        posts(query: String): [Post!]!
+        comments: [Comment!]!
     } 
     
     type User {
@@ -12,6 +86,8 @@ const typeDefs = gql`
         name: String!
         email: String!
         age: Int
+        posts: [Post]!
+        comments: [Comment]!
     }
     
     type Post {
@@ -19,20 +95,65 @@ const typeDefs = gql`
         title: String!
         body: String!
         published: Boolean!
+        author: User!
+        comments: [Comment]!
+    }
+    
+    type Comment {
+        id: ID!
+        comment: String!
+        author: User!
+        post: Post!
     }
 `
 
 // Resolvers
 const resolvers = {
     Query: {
-        me() {
-            return {
-                
-            }
+        users(parent, {query}, context, info) {
+            return users;
         },
 
-        post() {
+        posts(parent, {query}, context, info) {
+            if (!query) {
+                return posts;
+            }
 
+            return posts.filter(post => post.title.toLocaleLowerCase().includes(query.toLocaleLowerCase()))
+        },
+
+        comments() {
+            return comments;
+        },
+    },
+
+    Post: {
+        author(parent, args, context, info) {
+            return users.find(user => user.id === Number(parent.author));
+        },
+
+        comments(parent, args, context, info) {
+            return comments.filter(comment => comment.post === parent.id);
+        }
+    },
+
+    User: {
+        posts(parent, args, context, info) {
+            return posts.filter(post => Number(post.author) === parent.id);
+        },
+
+        comments(parent, args, context, info) {
+            return comments.filter(comment => Number(comment.author) === parent.id);
+        }
+    },
+
+    Comment: {
+        author(parent, args, context, info) {
+            return users.find(user => user.id === Number(parent.author));
+        },
+
+        post(parent, args, context, info) {
+            return posts.find(post => post.id === parent.post);
         }
     }
 }
