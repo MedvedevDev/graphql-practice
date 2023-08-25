d41c9b67171c4090ba7c05301485bfb269733039
MedvedevDev <a95.medvedev@gmail.com>
2023-08-25 11:56:49 +0300

    Update post and comment mutation

--- blogg-app-2/src/resolvers/Mutation.js
+++ blogg-app-2/src/resolvers/Mutation.js
@@ -21,34 +21,6 @@ const Mutation = {
         return user;
     },
 
-    deleteUser(parent, args, { db }, info) {
-        const userIndex = db.users.findIndex(user => user.id === args.id)
-
-        if (userIndex === -1) {
-            throw new Error('User not found')
-        }
-
-        // Delete user by splice method (returns array of deleted elements)
-        const removed = db.users.splice(userIndex, 1);
-
-        // Delete all associated posts
-        db.posts = db.posts.filter(post => {
-            const match = post.author === args.id;
-
-            if (match) {
-                // Delete comments in the post
-                db.comments = db.comments.filter(comment => comment.post !== post.id);
-            }
-
-            return !match;
-        })
-
-        // Delete all associated comments
-        db.comments = db.comments.filter(comment => comment.author !== args.id);
-
-        return removed[0];
-    },
-
     updateUser(parent, { id, data }, { db }, info) {
         const user = db.users.find(user => user.id === id);
 
@@ -81,6 +53,34 @@ const Mutation = {
         return user;
     },
 
+    deleteUser(parent, args, { db }, info) {
+        const userIndex = db.users.findIndex(user => user.id === args.id)
+
+        if (userIndex === -1) {
+            throw new Error('User not found')
+        }
+
+        // Delete user by splice method (returns array of deleted elements)
+        const removed = db.users.splice(userIndex, 1);
+
+        // Delete all associated posts
+        db.posts = db.posts.filter(post => {
+            const match = post.author === args.id;
+
+            if (match) {
+                // Delete comments in the post
+                db.comments = db.comments.filter(comment => comment.post !== post.id);
+            }
+
+            return !match;
+        })
+
+        // Delete all associated comments
+        db.comments = db.comments.filter(comment => comment.author !== args.id);
+
+        return removed[0];
+    },
+
     createPost(parent, args, { db }, info) {
         const userExists = db.users.some((user) => user.id === args.data.author)
 
@@ -96,6 +96,28 @@ const Mutation = {
         db.posts.push(post)
 
         return post
+    },
+
+    updatePost(parent, { id, data }, { db }, info) {
+        const post = db.posts.find(post => post.id === id);
+
+        if (!post) {
+            throw new Error("Post not found");
+        }
+
+        if (typeof data.title === 'string') {
+            post.title = data.title;
+        }
+
+        if (typeof data.body === 'string') {
+            post.body = data.body;
+        }
+
+        if (typeof data.published === 'boolean') {
+            post.published = data.published;
+        }
+
+        return post;
     },
 
     deletePost(parent, args, { db }, info) {
@@ -128,6 +150,20 @@ const Mutation = {
         };
 
         db.comments.push(comment);
+
+        return comment;
+    },
+
+    updateComment(parent, { id, data }, { db }, info) {
+        const comment = db.comments.find(cmnt => cmnt.id === id);
+
+        if (!comment) {
+            throw new Error("Comment not found");
+        }
+
+        if (typeof data.text === 'string') {
+            comment.text = data.text;
+        }
 
         return comment;
     },
